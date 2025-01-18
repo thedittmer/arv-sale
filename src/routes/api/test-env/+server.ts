8649637a0151e592869f7e155ea58efb4c45d141
Jason Dittmer <jason@zrimarketing.com>
2025-01-18 08:32:56 -0600

    remove api test

--- src/routes/api/test-env/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { PRIVATE_EMAIL, PRIVATE_SENDGRID_API_KEY } from '$env/static/private';
-
-export function GET() {
-    return json({
-        email: PRIVATE_EMAIL ? 'set' : 'missing',
-        apiKey: PRIVATE_SENDGRID_API_KEY ? 'set' : 'missing'
-    });
-} 
